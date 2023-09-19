866442dfbd3d4eb30f5f8e888f543cc84408a8a5
TripleSoi <electroguitar13@gmail.com>
2023-09-19 14:06:35 +0300

    Deleting some code Unzip.java

--- Unzip.java
+++ Unzip.java
@@ -17,24 +17,7 @@ public class Unzip {
 		 	else
 		 		outputPath+=File.separator;
 		 	
-	    	// 1.0 Create output directory
-	    	File outputDirectory = new File(outputPath);
-	    	
-	    	if(outputDirectory.exists())
-	    		outputDirectory.delete();
-	    		
-	    	outputDirectory.mkdir();
-	    	
-
-	    	// 2.0 Unzip (create folders & copy files)
-	    	try {
-
-	    		// 2.1 Get zip input stream
-	    		ZipInputStream zip = new ZipInputStream(new FileInputStream(zipFile));
-				
-				ZipEntry entry = null;
-				int len;
-				byte[] buffer = new byte[1024];
+	
 				
 				// 2.2 Go over each entry "file/folder" in zip file
 				while((entry = zip.getNextEntry()) != null){
